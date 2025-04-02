27a77a8fdb18d7b6849cfa3658a591b9cc2ab969
Jinsung <baek6529@gmail.com>
2025-04-02 15:05:16 +0900

    2.2 완료

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,12 +3,12 @@
 # Exercise 1.27
 import csv
 import sys
-from fileinput import filename
 
 
 def portfolio_cost(filename):
     'csv 파일 이름을 넣으면 포트폴리오 총 비용을 부동소수점으로 반환'
-    total_price = float()
+    total_cost = 0.0
+
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         next(rows)
@@ -17,12 +17,12 @@ def portfolio_cost(filename):
             try:
                 shares = int(row[1])
                 price = float(row[2])
-                total_price = total_price + (shares * price)
+                total_cost = total_cost + (shares * price)
             except ValueError:
                 print("value error with this line:", row)
                 continue
 
-    return total_price
+    return total_cost
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,52 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    '''포트폴리오 파일의 총 비용(주식수*가격)을 계산'''
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for line in rows:
+            row = {
+                'name': line[0],
+                'shares': int(line[1]),
+                'price': float(line[2])
+            }
+            portfolio.append(row)
+
+    return portfolio
+
+def read_prices(filename):
+    '''가격정보를 읽기'''
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for line in rows:
+            try:
+                prices[line[0]] = float(line[1])
+            except IndexError:
+                print('IndexError:', line)
+                continue
+            print(line)
+
+    return prices
+
+def analyze_portfolio(portfolio, prices):
+
+    portfolio_current_value = 0.0
+    total_earned = 0.0
+    for asset in portfolio:
+        asset_name = asset['name']
+        asset_shares = asset['shares']
+        asset_price = asset_shares * asset['price']
+        asset_current_price = asset_shares * prices[asset_name]
+        # print(f'{asset_name} | 구매가치:{asset_price} / 현재가치:{asset_current_price} / 손익:{asset_current_price - asset_price}')
+        portfolio_current_value += asset_current_price
+        total_earned += asset_current_price - asset_price
+
+    print(f'포트폴리오 현재가치: {portfolio_current_value} / 손익: {total_earned}')
