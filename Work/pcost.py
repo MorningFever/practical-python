a3d391c860705148b6c7d1d84cef5f7ac2ff773f
Jinsung <baek6529@gmail.com>
2025-04-02 11:11:09 +0900

    1.7 완료

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,39 @@
 # pcost.py
 #
 # Exercise 1.27
-import os
+import csv
+import sys
+from fileinput import filename
 
 
-total_price = float()
+def portfolio_cost(filename):
+    'csv 파일 이름을 넣으면 포트폴리오 총 비용을 부동소수점으로 반환'
+    total_price = float()
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
 
-with open('Data/portfolio.csv', 'rt') as f:
-    next(f)
-    for line in f:
-        row = line.split(',')
-        shares = int(row[1])
-        price = float(row[2])
-        total_price = total_price + (shares*price)
+        for row in rows:
+            try:
+                shares = int(row[1])
+                price = float(row[2])
+                total_price = total_price + (shares * price)
+            except ValueError:
+                print("value error with this line:", row)
+                continue
 
-print("Total cost", total_price)
+    return total_price
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print("Total cost", cost)
+
+
+
+
+
+
